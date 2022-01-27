ebed13551fc8877511f2dcd53aa69c6769baa0d7
陈正豪 <chenzhenghaopeter@163.com>
2022-01-27 20:02:25 +0800

    Linux-practice

---
+++ Linux/day27/ntohs.c
@@ -0,0 +1,12 @@
+#include <func.h>
+
+int main()
+{
+    uint16_t port=0x1234,nport;
+    nport=htons(port);//把主机字节序转为网络字节序
+    printf("nport=%x\n",nport);
+    port=0;
+    port=ntohs(nport);//把网络字节序转为主机字节序
+    printf("port=%x\n",port);
+    return 0;
+}
